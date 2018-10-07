7f21ca97cb7a2ce88691cb69459d3b8ac61e4eb6
José Raimundo Barbosa <j.zmais@gmail.com>
2018-10-06 21:30:19 -0300

    imagens e atividade das camadas

--- atividade4/src/main.cpp
+++ atividade4/src/main.cpp
@@ -2,17 +2,22 @@
 #include <stdio.h>
 #include <bitset>
 #include <iostream>
+#include <string>
+#include <sstream>
+
+#define MAX_BIT 8
 
 using namespace std;
 
 int main(int argc, char const *argv[]){
 	
-	if( argc != 2)  {
+	if( argc < 2)  {
      cout <<" Usage: display_image ImageToLoadAndDisplay" << endl;
      return -1;
     }
 
-    Mat src, output;
+    Mat src;
+	
     src = imread(argv[1], CV_LOAD_IMAGE_COLOR);  
 
     if(! src.data )  {
@@ -20,42 +25,52 @@ int main(int argc, char const *argv[]){
         return -1;
     }
 
-	output = Mat(src.rows, src.cols, CV_8UC3);  
+	//carregando camadas
+	vector<Mat> layers;
+	vector<uint8_t*> destiny;
+	
+	for(size_t i = 0; i < MAX_BIT; i++){
+		layers.push_back(Mat(src.rows, src.cols, CV_8UC3));
+		destiny.push_back((uint8_t*)layers[i].data);
+	}
+	
+ 	uint8_t *origin 	= (uint8_t*)src.data;
+	
+	
+	bitset<8> byte_in_r, byte_in_g, byte_in_b, 
+			  byte_out_r,byte_out_g,byte_out_b ;
 
-	uint8_t *origin 	= (uint8_t*)src.data;
-	uint8_t *destino 	= (uint8_t*)output.data;
-
-
-	bitset<8> byte_in, byte_out;
-	// bitset<16> invert_valor;
 	for(int i = 0; i < src.rows; i++){
 		for(int j = 0; j < src.cols; j++){
-			byte_in = origin[i*src.cols*src.channels() + j*src.channels() + 0];
-			
-			for(int k = 7; k >= 4; k--){
-				byte_out[k] = byte_in[k];
+			byte_in_b = origin[i*src.cols*src.channels() + j*src.channels() + 0];
+			byte_in_g = origin[i*src.cols*src.channels() + j*src.channels() + 1];
+			byte_in_r = origin[i*src.cols*src.channels() + j*src.channels() + 2];
+					
+			for(size_t k = 0; k < MAX_BIT; k++){
+				byte_out_r.reset();
+				byte_out_g.reset();
+				byte_out_b.reset();
+				byte_out_r[k] = byte_in_r[k];
+				byte_out_g[k] = byte_in_g[k];
+				byte_out_b[k] = byte_in_b[k];
+				destiny[k][i*layers[k].cols*src.channels() + j*src.channels() + 0] = byte_out_b.to_ulong();
+				destiny[k][i*layers[k].cols*src.channels() + j*src.channels() + 1] = byte_out_g.to_ulong();
+				destiny[k][i*layers[k].cols*src.channels() + j*src.channels() + 2] = byte_out_r.to_ulong();
 			}
-			destino[i*output.cols*src.channels() + j*src.channels() + 0] = byte_out.to_ulong();
 			
-			// cout <<"Origin: 	" << byte_in << endl; 
-
-			// cout <<"Destino: 	" << byte_out << endl; 
-
-			//blue
-			//destino[i*output.cols*src.channels() + j*src.channels() + 0] = origin[i*src.cols*src.channels() + j*src.channels() + 0];
-			//green
-			//destino[i*output.cols*src.channels() + j*src.channels() + 1] = origin[i*src.cols*src.channels() + j*src.channels() + 1];
-			//red
-			//destino[i*output.cols*src.channels() + j*src.channels() + 2] = origin[i*src.cols*src.channels() + j*src.channels() + 2];
 		}
 	}
 
-		
-	// cout << byte_in << endl;	//0010110100000000
+	
+	for(size_t i = 0; i < MAX_BIT; i++)	{
+		string nome = "";
+		std::stringstream sstm;
+		sstm << "Janela" << i;
+		nome = sstm.str();
 
-   imshow( "Display window", output );                   // Show our image inside it.
-
-
+		imshow( nome, layers[i] );
+	}
+	
 	waitKey(0);     
 	return 0;
 }
